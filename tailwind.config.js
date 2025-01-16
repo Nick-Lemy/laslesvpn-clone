05742f721d93dd088cf22a7d0e7399683f56f443
Nick-Lemy <n.kayiranga@alustudent.com>
2025-01-16 10:05:30 +0200

    First section finally done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      redaccented: "#f53838",
+      white: "#fff",
+      gray: colors.gray,
+    },
     extend: {},
   },
   plugins: [],
